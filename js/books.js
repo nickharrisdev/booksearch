2ece8e2106d2cd77d49acc403d05374bdc3a6575
Nicholas Harris <nharris614@gmail.com>
2019-01-28 17:23:56 -0600

    Added feature to request data from the Books API

--- js/books.js
+++ js/books.js
@@ -1,18 +1,37 @@
 $(document).ready(function(){
 
-
 	$("#form").submit(function(){
 
-		var search = $("#book").val();
+		var searchTerms = $("#book").val();
 
-		if (search == '') {
+		if (searchTerms === '') {
 
-			alert ("Please enter something in the field to search.");
+			alert ("Please enter text into the search field.");
+
+		} else {
+
+			var title = "";
+			var author = "";
+			var img = "";
+			var url = "";
+
+			
+			$.get("https://www.googleapis.com/books/v1/volumes?q=" + searchTerms, function(response){
+				console.log(response);
+				/*gets book title
+				for(i=0; i<response.items.length; i++) {
+
+					title = $("<h5>" + response.items[i].volumeInfo.title + "</h5>");
+
+				} */
+
+				
+
+			});
 
 		}
 
-	});
-
 	return false;
 
+	});
 });
