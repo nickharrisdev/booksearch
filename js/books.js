85d24c2fba119c3fad0045033515ec3ac38121db
Nicholas Harris <nharris614@gmail.com>
2019-01-28 18:07:54 -0600

    Added code to display the data fetched from Google

--- js/books.js
+++ js/books.js
@@ -18,14 +18,24 @@ $(document).ready(function(){
 			
 			$.get("https://www.googleapis.com/books/v1/volumes?q=" + searchTerms, function(response){
 				console.log(response);
-				/*gets book title
+				
+				// gets book info 
+
 				for(i=0; i<response.items.length; i++) {
 
 					title = $("<h5>" + response.items[i].volumeInfo.title + "</h5>");
+					author = $("<h5>" + response.items[i].volumeInfo.authors + "</h5>");
+					img = $("<img class='bookcover'><br><a href=" + response.items[i].volumeInfo.infoLink + "><button id='cover-button'>More info</button></a>")
+					url = response.items[i].volumeInfo.imageLinks.thumbnail;
 
-				} */
+					img.attr("src", url); 
 
-				
+					title.appendTo("#results");
+
+					author.appendTo("#results");
+					
+					img.appendTo("#results");
+				}
 
 			});
 
