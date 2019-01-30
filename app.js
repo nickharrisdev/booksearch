d3d4c9ea2f56e51389f3d171185f8097a26dec21
Nicholas Harris <nharris614@gmail.com>
2019-01-30 14:24:13 -0600

    Updated connection between script file and html

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html', 'css/app.css', 'app/books.js'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(process.env.PORT || 4000, function(){
