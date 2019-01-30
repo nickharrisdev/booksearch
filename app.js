afe36c22c89357b34d6332288dcedeb6a55a9192
Nicholas Harris <nharris614@gmail.com>
2019-01-30 14:18:36 -0600

    Updated app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html', 'css/app.css', 'app/books.js'));
 });
 
 app.listen(process.env.PORT || 4000, function(){
