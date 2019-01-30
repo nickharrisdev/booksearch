b0c1218fa656db7293edd54f65fd054e1f04c7a5
Nicholas Harris <nharris614@gmail.com>
2019-01-30 13:48:33 -0600

    Continuing to correct deployment

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require(‘express’);
+var port = process.env.PORT || 3000;
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get(‘/’, function (req, res) {
+ res.send(JSON.stringify({ Hello: ‘World’}));
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, function () {
+ console.log(`Example app listening on port !`);
+});
