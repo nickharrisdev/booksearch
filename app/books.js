accc231f24bd5a1810763e9c3dc2d663f507aa76
Nicholas Harris <nharris614@gmail.com>
2019-02-12 13:35:22 -0600

    Added a "clear results" button, organized CSS file

--- app/books.js
+++ app/books.js
@@ -1,68 +1,69 @@
 //---initial refactor---//
 
-	var title = "";
-	var author = "";
-	var publisher = "";
-	var img = "";
-	var url = "";
+var title = "";
+var author = "";
+var publisher = "";
+var img = "";
+var url = "";
 
-	function init () {
+function init () {
 
-		$("#form").submit(function(){
+	$("#form").submit(function(){
 
-			var searchTerms = $('#book').val();
+		var searchTerms = $('#book').val();
 
-			if (searchTerms === '') {
+		if (searchTerms === '') {
 
-				alert ("Please enter text into the search field.");
+			alert ("Please enter text into the search field.");
 
-			} else {
+		} else {
 
-				///call a function here
-				getBookinfo(searchTerms);
+			///call a function to retrieve book info from google
+			getBookinfo(searchTerms);
 
-			}
+		}
 
-		return false;
+	return false;
 
-		});
-	}
+	});
+}
 
-	function getBookinfo(searchTerms) {
+function getBookinfo(searchTerms) {
+			
+	$.get("https://www.googleapis.com/books/v1/volumes?q=" + searchTerms, function(response){
+		//log the response as a test
+		console.log(response);
 				
-		$.get("https://www.googleapis.com/books/v1/volumes?q=" + searchTerms, function(response){
-			console.log(response);
+		// for loop to get relevant book info from JSON file
+
+		for(i=0; i<response.items.length; i++) {
+
+			title = $("<h3>" + response.items[i].volumeInfo.title + "</h3>");
+			author = $("<h5>Author(s): " + response.items[i].volumeInfo.authors + " </h5>");
+			publisher = $("<h5>Publisher: " + response.items[i].volumeInfo.publisher + "</h5>");
+			img = $("<img class='bookcover'><br><a href=" + response.items[i].volumeInfo.infoLink + "><button id='cover-button'>More Info</button></a>")
+			url = response.items[i].volumeInfo.imageLinks.thumbnail;
+
+			//call function to append 
+			appendResults();
+
+		}
+	});
+}
+
+function appendResults() {
+	
+	img.attr("src", url); 
+
+	title.appendTo("#results");
+
+	author.appendTo("#results");
+
+	publisher.appendTo("#results");
 					
-			// for loop to get relevant book info from JSON file
+	img.appendTo("#results");
+}
 
-			for(i=0; i<response.items.length; i++) {
-
-				title = $("<h3>" + response.items[i].volumeInfo.title + "</h3>");
-				author = $("<h5>Author(s): " + response.items[i].volumeInfo.authors + " </h5>");
-				publisher = $("<h5>Publisher: " + response.items[i].volumeInfo.publisher + "</h5>");
-				img = $("<img class='bookcover'><br><a href=" + response.items[i].volumeInfo.infoLink + "><button id='cover-button'>More Info</button></a>")
-				url = response.items[i].volumeInfo.imageLinks.thumbnail;
-
-				//call function to append 
-				appendResults();
-
-			}
-		});
-	}
-
-	function appendResults() {
-		
-		img.attr("src", url); 
-
-		title.appendTo("#results");
-
-		author.appendTo("#results");
-
-		publisher.appendTo("#results");
-						
-		img.appendTo("#results");
-	}
-
-	init();
+init();
 
 
